2d95e2299ad1e0da51f434e88a7455c70b827efc
nikhithabandi <nikhithabandi256@gmail.com>
2026-07-30 11:47:36 +0530

    msg

--- Practice/M01_Sliding_Window_Two_Pointers/S03/Variable_Sliding_Window.py
+++ Practice/M01_Sliding_Window_Two_Pointers/S03/Variable_Sliding_Window.py
@@ -1,4 +1,4 @@
-# minimum size subarray sum
+# 713 minimum size subarray sum
 class Solution:
     def minSubArrayLen(self, target: int, nums: list[int]) -> int:
         left = 0
@@ -14,3 +14,24 @@ class Solution:
                 left += 1
 
         return 0 if min_len == float('inf') else min_len
+
+#904 subarray product less than k
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        left = 0
+        product = 1
+        count = 0
+
+        for right in range(len(nums)):
+            product *= nums[right]
+
+            while product >= k:
+                product //= nums[left]
+                left += 1
+
+            count += (right - left + 1)
+
+        return count
